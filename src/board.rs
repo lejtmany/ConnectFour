85113b0c9e654a36ffd37b4960607a39f975b5a7
Joseph Lejtman <lejtmany@gmail.com>
2018-06-20 23:25:51 -0400

    board ctor

--- src/board.rs
+++ src/board.rs
@@ -1,11 +1,19 @@
 struct Board {
-    board:[[Square;7];6]
+    board:[[Square;6];7]
 }
 
+#[derive(Copy, Clone)]
 struct Square {
-    square: Option<Team>
+    team: Option<Team>
 }
 
+#[derive(Copy, Clone)]
 enum Team{
     TEAM1, TEAM2
+}
+
+impl Board {
+    fn new()->Board{
+        Board{board:[[Square{team:None};6];7]}
+    }
 }
