0117d50d5855e8ca6bdd188fb34196ca8bd4d6f7
Joseph Lejtman <lejtmany@gmail.com>
2018-07-01 15:18:28 -0400

    insert at column logic

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,12 @@
 extern crate connect_four;
 use connect_four::board::Board;
+use connect_four::board::Team;
 
 fn main(){
-    let board = Board::new();
+    let mut board = Board::new();
+    board.print_board();
+    board.insert_at_column(3, Team::TEAM1);
+    board.insert_at_column(3, Team::TEAM2);
+    board.insert_at_column(2, Team::TEAM1);
     board.print_board();
 }
--- src/board.rs
+++ src/board.rs
@@ -2,27 +2,22 @@ const BOARD_HEIGHT:usize = 6;
 const BOARD_WIDTH:usize = 7;
 
 pub struct Board {
-    board:[[Square;BOARD_WIDTH];BOARD_HEIGHT],
+    board:[[Option<Team>;BOARD_WIDTH];BOARD_HEIGHT],
 }
 
 #[derive(Copy, Clone)]
-struct Square {
-    team: Option<Team>
-}
-
-#[derive(Copy, Clone)]
-enum Team{
+pub enum Team{
     TEAM1, TEAM2
 }
 
 impl Board {
     pub fn new()->Board{
-        Board{board:[[Square{team:None};BOARD_WIDTH];BOARD_HEIGHT]}
+        Board{board:[[None;BOARD_WIDTH];BOARD_HEIGHT]}
     }
 
     pub fn print_board(&self){
 
-        for column_num in 1..self.board[0].len() + 1 {
+        for column_num in 0..self.board[0].len() {
             print!("{}",column_num )
         }
         println!();
@@ -30,7 +25,7 @@ impl Board {
 
         for row in self.board.iter(){
             for column in row.iter(){
-                let team_str = match column.team {
+                let team_str = match column {
                     Some(Team::TEAM1) => '1',
                     Some(Team::TEAM2) => '2',
                     None => '0',
@@ -38,6 +33,18 @@ impl Board {
                 print!("{}",team_str);
             }
             println!("")
+        }
+    }
+
+    pub fn insert_at_column(&mut self, column_num:usize, team:Team){
+        if column_num > BOARD_WIDTH {
+            panic!("column number is outside of range. must be between 0 and {} but was {}", BOARD_WIDTH, column_num);
+        }
+        for row_num in 0..BOARD_HEIGHT + 1{
+            if row_num >= BOARD_HEIGHT || self.board[row_num][column_num].is_some(){
+                self.board[row_num - 1][column_num] = Some(team);
+                break;
+            }
         }
     }
 }
